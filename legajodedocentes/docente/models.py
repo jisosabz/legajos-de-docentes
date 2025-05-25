d5a4d0929d6c522636b9ba1bc65bc597b57582d7
jisosabz <jisosabz@users.noreply.github.com>
2025-05-24 21:34:24 -0300

    nuevo campo al models de notificaciones

--- legajodedocentes/docente/models.py
+++ legajodedocentes/docente/models.py
@@ -100,7 +100,7 @@ class Notificacion(models.Model):
     documento = models.ForeignKey(Documento, on_delete=models.CASCADE, null=True)
     fecha_envio = models.DateTimeField(auto_now_add=True)
     estado = models.CharField(max_length=20, choices=ESTADO_OPCIONES, default='Pendiente')
-    fecha_resolucion = models.DateTimeField(null=True, blank=True)  # <- este campo nuevo
+    fecha_resolucion = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return f"Notificación: {self.documento.nombre if self.documento else 'Sin documento'} - {self.estado}"
